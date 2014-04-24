b7b31baa0593ba3745815def29e9731bdaa551d8
Aaron Torres <torresaa@msu.edu>
2014-04-24 09:35:45 -0400

    Fixed exception when game is won.

--- StepDX/StepDX/Game.cs
+++ StepDX/StepDX/Game.cs
@@ -329,6 +329,7 @@ namespace StepDX
                             if (p is FinishPlat)
                             {
                                 GameWon();
+                                return;
                             }
                         }
 
